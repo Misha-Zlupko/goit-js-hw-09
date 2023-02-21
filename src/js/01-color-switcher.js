3a482f8e0a59413a046f6af414ef7db3d1ae6745
mafioz359 <64733979+mafioz359@users.noreply.github.com>
2023-02-21 19:23:54 +0300

    aadsd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let stopColor = null;
 
+function disableButton(btn) {
+  btn.disabled = true;
+}
+
+function enableButton(btn) {
+  btn.disabled = undefined;
+}
+
+function changeBackroundColor(color = '#ffffff') {
+  document.body.style.backgroundColor = color;
+}
+
+startButton.addEventListener('click', () => {
+  disableButton(startButton);
+  enableButton(stopButton);
+  stopColor = setInterval(() => {
+    changeBackroundColor(getRandomHexColor());
+  }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  disableButton(stopButton);
+  enableButton(startButton);
+  clearInterval(stopColor);
+  changeBackroundColor();
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        return resolve({ position, delay });
+      } else {
+        return reject({ position, delay });
+      }
+    });
+  }, delay);
+}
+
+const formEl = document.querySelector('.form');
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const { target } = evt;
+
+  const delay = target.delay.value;
+  const step = target.step.value;
+  const amount = target.amount.value;
+
+  for (let i = 0; i < amount; i++) {
+    const resoultingDelay = Number(delay) + step * i;
+    createPromise(i + 1, resoultingDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+formEl.addEventListener('submit', onFormSubmit);
